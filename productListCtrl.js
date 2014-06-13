96b70fb8cf630c146d0c968c84b51eb02c04ee8c
Bernard Labno <bernard.labno@itcrowd.pl>
2014-06-13 10:03:13 +0200

    Two way binding

--- productListCtrl.js
+++ productListCtrl.js
@@ -1,11 +1,21 @@
 function ProductListCtrl($scope)
 {
     var products = [
-        {id: 1, name: 'TV'}
+        {id: 1, name: 'TV'},
+        {id: 2, name: 'Fridge'}
     ];
+
+    $scope.filter = '';
 
     $scope.getProducts = function ()
     {
-        return products;
+        var results = [];
+        angular.forEach(products, function (value)
+        {
+            if (value.name.match($scope.filter)) {
+                results.push(value);
+            }
+        });
+        return results;
     }
 }
